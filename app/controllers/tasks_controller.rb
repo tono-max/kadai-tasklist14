ffe9c736e76f20cc273294890cb03005155cde3e
tono <tonton.kirawaku@gmail.com>
2020-07-26 13:39:27 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,18 +2,18 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.all
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
-      @task = Task.new
+    @task = Task.new
   end
 
   def create
-      @task = Task.new(task_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -28,8 +28,6 @@ class TasksController < ApplicationController
   end
 
   def update
-      set_task
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
