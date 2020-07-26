13f98d2a78d47ff73f664c46afe050b60681486a
tono <tonton.kirawaku@gmail.com>
2020-07-26 07:18:01 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
   end
 
   def update
-       @task = task.find(params[:id])
+      @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
