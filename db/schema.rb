1c702bf28e0daf9e2c3bf97c47057d363e0cd10b
tono <tonton.kirawaku@gmail.com>
2020-07-31 02:48:05 +0000

    kadai-tasklist2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_135104) do
+ActiveRecord::Schema.define(version: 2020_07_27_092042) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "status"
+    t.string "status"
   end
 
 end
